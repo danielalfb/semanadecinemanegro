8799fbc2bb2c8556bb49e89b546a801fbcfd5c3c
Lidiane Mara <lidianemara93@gmail.com>
2023-09-09 10:24:08 -0300

    add new color tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,8 @@ module.exports = {
       'green-500': '#00392E',
       'green-100': '#6AA924',
       'yellow-500': '#F29F05',
-      'yellow-100': '#F2CB05'
+      'yellow-100': '#F2CB05',
+      'slate-800': 'rgb(30 41 59)',
     },
     fontFamily: {
         sans: ["Epilogue, sans-serif"]
