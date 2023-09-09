18bdb5ef25e763fe141c0a1dd25baeac135fc6c9
Lidiane Mara <lidianemara93@gmail.com>
2023-09-09 09:45:25 -0300

    add homenagem session

--- pages/mostras/homenagem/index.js
+++ pages/mostras/homenagem/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import ContentMenu from "../../../components/ContentMenu";
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
+import MovieCard from "../../../components/MovieCard";
+import MoviesGroup from "../../../components/MoviesGroup";
+import { HOMENAGEM } from "../../../helpers/homenagem";
 
 export default function homenagem() {
   return (
@@ -9,7 +11,15 @@ export default function homenagem() {
       <Header />
       <div className='mt-36 p-10 w-full laptop:mt-30'>
         <h1 className='mb-6'>HOMENAGEM - MARIA JOSÉ NOVAIS OLIVEIRA</h1>
-        <h2>EM BREVE</h2>{" "}
+        <MoviesGroup>
+          {HOMENAGEM.map((movie) => (
+            <MovieCard
+              key={movie.title}
+              movie={movie}
+              color='var(--clr-blue-dark)'
+            />
+          ))}
+        </MoviesGroup>
       </div>
       <Footer />
     </div>
