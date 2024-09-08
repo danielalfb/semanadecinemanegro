9168717616ac101faaa292358ae6eca812e32cc8
Daniela Barbosab <danielalfbarbosa@gmail.com>
2024-09-08 17:06:22 +0100

    add new home page, still working on it

--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,24 @@ export default function Home() {
       </Head>
 
       <div
-        className="relative h-screen w-full bg-top bg-cover flex justify-end items-center"
+        className="relative h-screen w-full bg-top bg-cover flex justify-end items-start p-10"
         style={{
           backgroundImage: `url('/images/semana_banner_home.png')`,
         }}
       >
         <div className="temp-page">
-          <span>em breve</span>
-          <div className="more-info">
+          <span className="text-blue-500 text-8xl">em breve</span>
+          <div className="more-info text-2xl">
             para mais informações acesse
             <Link
               passHref={true}
               href="https://www.instagram.com/semana.cinemanegrobh?igsh=MWN0d2szN2p4Njdjaw=="
             >
-              <a target="_blank" className="text-purple-500 hover:shadow-sm">
-                {" "}  @semana.cinemanegrobh{" "}
+              <a
+                target="_blank"
+                className="text-blue-700 font-bold hover:shadow-sm"
+              >
+                @semana.cinemanegrobh
               </a>
             </Link>
           </div>
