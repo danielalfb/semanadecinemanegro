e6b030d459ac3a434b71a6335375f2e6939aed9c
Lidiane Mara <lidianemara93@gmail.com>
2024-09-13 21:59:43 +0000

    feat:initial editions

--- pages/anteriores/index.js
+++ pages/anteriores/index.js
@@ -11,7 +11,119 @@ export default function anteriores() {
       <Shape isLastSeasons color='var(--clr-blue-lightest)' />
       <Header />
       <div className='mt-36 p-10 w-full laptop:mt-30'>
-        <h1 className='mb-6'>EDIÇÕES ANTERIORES</h1>
+        <div class="title-container">
+          <div id="title">
+              <h1>Edições anteriores</h1>
+            </div>
+        </div>
+        <div className='flex flex-col w-full mb-6 p-4 rounded-sm border-4'>
+          <div className='flex w-full justify-between items-center mb-6'>
+            <h2>SCNBH&apos;23</h2>
+            <a
+              target='_blank'
+              href="/catalogo_2_edicao.pdf"
+              rel="noopener noreferrer"
+              download
+              className='text-blue-500 bg-transparent text-sm p-2 border-2 rounded-lg uppercase font-bold hover:shadow-sm'
+            >
+              Baixar catálogo
+            </a>
+            {/* <Link
+              href='/files/catalogo_2_edicao.pdf'
+              locale={false}
+              rel="noopener noreferrer"
+              target="_blank"
+              download="files/catalogo_2_edicao.pdf"
+            >
+            </Link> */}
+          </div>
+          <p>
+            A 2ª edição da Semana de Cinema Negro de Belo Horizonte aconteceu
+            presencialmente pela primeira vez e, por esse motivo, a presença do
+            público foi essencial para a construção do festival. Promovemos um
+            encontro marcado pelo afeto e acolhimento. Apresentamos, de 8 a 15
+            de setembro de 2022, um conjunto composto por 53 filmes de
+            cinematografias africanas, negras brasileiras e da diáspora,
+            distribuído em três mostras, duas homenagens e uma Sessão Especial:{" "}
+            <i>
+              Por Outros Cinemas Africanos; Med Hondo: cinema e liberadade;
+              Cine-Escrituras Pretas; Sessão Homenagem Maria José Novais
+              Oliveira: Rejane Faria – no palco e nas telas; Semana de Cinema
+              Negro convida o NICHO 54; e Ibejis (Infatil)
+            </i>
+            . As conversas com convidadas e convidados estão disponíveis no
+            canal do Youtube da Semana de Cinema Negro (acesse no{" "}
+            <Link
+              passHref={true}
+              href='https://www.youtube.com/@semanadecinemanegro/streams'
+            >
+              <a target='_blank' className='text-blue-300 flex-wrap'>
+                Youtube
+              </a>
+            </Link>
+            .) Concebemos um catálogo com textos que refletem sobre a mostra{" "}
+            <i>Por Outros Cinemas Africanos</i>. Além disso, trouxemos duas
+            entrevistas: uma com Annabelle Aventurin, publicada na revista{" "}
+            <i>Non Fiction</i>, sobre os processos de restauração dos filmes do
+            cineasta Med Hondo; e outra realizada por Alessandra Brito com
+            Rejane Faria, a nossa homenageada da edição. O catálogo está
+            disponível para download.
+          </p>
+        </div>
+        <div className='flex flex-col w-full mb-6 p-4 rounded-sm border-4'>
+          <div className='flex w-full justify-between items-center mb-6'>
+            <h2>SCNBH&apos;22</h2>
+            <a
+              target='_blank'
+              href="/catalogo_2_edicao.pdf"
+              rel="noopener noreferrer"
+              download
+              className='text-blue-500 bg-transparent text-sm p-2 border-2 rounded-lg uppercase font-bold hover:shadow-sm'
+            >
+              Baixar catálogo
+            </a>
+            {/* <Link
+              href='/files/catalogo_2_edicao.pdf'
+              locale={false}
+              rel="noopener noreferrer"
+              target="_blank"
+              download="files/catalogo_2_edicao.pdf"
+            >
+            </Link> */}
+          </div>
+          <p>
+            A 2ª edição da Semana de Cinema Negro de Belo Horizonte aconteceu
+            presencialmente pela primeira vez e, por esse motivo, a presença do
+            público foi essencial para a construção do festival. Promovemos um
+            encontro marcado pelo afeto e acolhimento. Apresentamos, de 8 a 15
+            de setembro de 2022, um conjunto composto por 53 filmes de
+            cinematografias africanas, negras brasileiras e da diáspora,
+            distribuído em três mostras, duas homenagens e uma Sessão Especial:{" "}
+            <i>
+              Por Outros Cinemas Africanos; Med Hondo: cinema e liberadade;
+              Cine-Escrituras Pretas; Sessão Homenagem Maria José Novais
+              Oliveira: Rejane Faria – no palco e nas telas; Semana de Cinema
+              Negro convida o NICHO 54; e Ibejis (Infatil)
+            </i>
+            . As conversas com convidadas e convidados estão disponíveis no
+            canal do Youtube da Semana de Cinema Negro (acesse no{" "}
+            <Link
+              passHref={true}
+              href='https://www.youtube.com/@semanadecinemanegro/streams'
+            >
+              <a target='_blank' className='text-blue-300 flex-wrap'>
+                Youtube
+              </a>
+            </Link>
+            .) Concebemos um catálogo com textos que refletem sobre a mostra{" "}
+            <i>Por Outros Cinemas Africanos</i>. Além disso, trouxemos duas
+            entrevistas: uma com Annabelle Aventurin, publicada na revista{" "}
+            <i>Non Fiction</i>, sobre os processos de restauração dos filmes do
+            cineasta Med Hondo; e outra realizada por Alessandra Brito com
+            Rejane Faria, a nossa homenageada da edição. O catálogo está
+            disponível para download.
+          </p>
+        </div>
         <div className='flex flex-col mb-6 w-full p-4 rounded-sm border-4'>
           <div className='flex w-full justify-between items-center mb-6'>
             <h2>SCNBH&apos;21</h2>
@@ -67,60 +179,6 @@ export default function anteriores() {
             .). Em nosso catálogo, que já está disponível para download, é
             possível conferir toda a programação, as sinopses dos filmes
             exibidos, assim como os textos publicados sobre essa edição.
-          </p>
-        </div>
-        <div className='flex flex-col w-full mb-6 p-4 rounded-sm border-4'>
-          <div className='flex w-full justify-between items-center mb-6'>
-            <h2>SCNBH&apos;22</h2>
-            <a
-              target='_blank'
-              href="/catalogo_2_edicao.pdf"
-              rel="noopener noreferrer"
-              download
-              className='text-blue-500 bg-transparent text-sm p-2 border-2 rounded-lg uppercase font-bold hover:shadow-sm'
-            >
-              Baixar catálogo
-            </a>
-            {/* <Link
-              href='/files/catalogo_2_edicao.pdf'
-              locale={false}
-              rel="noopener noreferrer"
-              target="_blank"
-              download="files/catalogo_2_edicao.pdf"
-            >
-            </Link> */}
-          </div>
-          <p>
-            A 2ª edição da Semana de Cinema Negro de Belo Horizonte aconteceu
-            presencialmente pela primeira vez e, por esse motivo, a presença do
-            público foi essencial para a construção do festival. Promovemos um
-            encontro marcado pelo afeto e acolhimento. Apresentamos, de 8 a 15
-            de setembro de 2022, um conjunto composto por 53 filmes de
-            cinematografias africanas, negras brasileiras e da diáspora,
-            distribuído em três mostras, duas homenagens e uma Sessão Especial:{" "}
-            <i>
-              Por Outros Cinemas Africanos; Med Hondo: cinema e liberadade;
-              Cine-Escrituras Pretas; Sessão Homenagem Maria José Novais
-              Oliveira: Rejane Faria – no palco e nas telas; Semana de Cinema
-              Negro convida o NICHO 54; e Ibejis (Infatil)
-            </i>
-            . As conversas com convidadas e convidados estão disponíveis no
-            canal do Youtube da Semana de Cinema Negro (acesse no{" "}
-            <Link
-              passHref={true}
-              href='https://www.youtube.com/@semanadecinemanegro/streams'
-            >
-              <a target='_blank' className='text-blue-300 flex-wrap'>
-                Youtube
-              </a>
-            </Link>
-            .) Concebemos um catálogo com textos que refletem sobre a mostra{" "}
-            <i>Por Outros Cinemas Africanos</i>. Além disso, trouxemos duas
-            entrevistas: uma com Annabelle Aventurin, publicada na revista{" "}
-            <i>Non Fiction</i>, sobre os processos de restauração dos filmes do
-            cineasta Med Hondo; e outra realizada por Alessandra Brito com
-            Rejane Faria, a nossa homenageada da edição. O catálogo está
-            disponível para download.
           </p>
         </div>
       </div>
