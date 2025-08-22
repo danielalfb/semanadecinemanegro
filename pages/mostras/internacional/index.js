764fe02d2d5a1e9c11e4f7d3e8d257a1cdc051ff
Lidiane Mara <lidianemara93@gmail.com>
2025-08-22 01:10:57 +0000

    feat: add default pages

--- pages/mostras/internacional/index.js
+++ pages/mostras/internacional/index.js
@@ -1,16 +1,15 @@
-import React from "react";
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
 import PageBanner from "../../../components/PageBanner";
 import { SITE_CONTENT } from '../../../helpers/content';
 
 
-export default function homenagem() {
-   return (
+export default function internacional() {
+  return (
         <div className="h-screen w-full relative overflow-x-hidden bg-white">
           <Header />
           <div className="w-full mt-[62px] desktop:mt-[120px]">
-            <PageBanner title={SITE_CONTENT.mostras[1].title}color={"var(--color-green)"} />
+            <PageBanner title={SITE_CONTENT.mostras[3].title}color={"var(--color-green)"} />
           </div>
           <Footer />
         </div>
